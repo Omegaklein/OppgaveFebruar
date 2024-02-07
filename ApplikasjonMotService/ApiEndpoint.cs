5dbaefd4601e3f94b021f70a6ddaa06f786c2989
RobinKopperud <Robin.kopperud@gmail.com>
2024-02-07 13:20:15 +0100

    siste fungerende

---
+++ ApplikasjonMotService/ApiEndpoint.cs
@@ -0,0 +1,17 @@
+﻿public class ApiEndpoint
+{
+    public string Url { get; set; }
+    public HttpMethod Method { get; set; }
+    public Dictionary<string, string> Headers { get; set; }
+    public object RequestBody { get; set; }
+    public Type ExpectedResponseType { get; set; }
+
+    public ApiEndpoint(string url, HttpMethod method)
+    {
+        Url = url;
+        Method = method;
+        Headers = new Dictionary<string, string>();
+        RequestBody = null;
+        ExpectedResponseType = typeof(string); // Default expected response type is string
+    }
+}
--- ApplikasjonMotService/Tokencall.cs
+++ ApplikasjonMotService/Tokencall.cs
@@ -1,43 +1,39 @@
-﻿using System;
-using System.Net.Http;
+﻿using Newtonsoft.Json.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
-namespace RobinOgMarius
+public class Tokencall
 {
-    public class Tokencall
+    public async Task<string> CallApi()
     {
-        public async Task CallApi()
+        string endepunkt = "Token";
+        string url = $"https://vbi.okonomibistand.no/VBInterfaceOBIDev/{endepunkt}";
+
+        Console.WriteLine("Enter username: ");
+        string username = Console.ReadLine();
+        Console.WriteLine("Enter password: ");
+        string password = Console.ReadLine();
+
+        using (HttpClient client = new HttpClient())
         {
-            string endepunkt = "Token";
-            string url = $"https://vbi.okonomibistand.no/VBInterfaceOBIDev/{endepunkt}"; // URL til APIet hvor token skal hentes
-            Console.WriteLine("Enter username: ");
-            string username = Console.ReadLine();
-            Console.WriteLine("Enter password: ");
-            string password = Console.ReadLine(); // Husk at passordet vises i klartekst i terminalen
-            // For å unngå dette, kan dere legge inn brukernavn og passord i User Secrets, dette forhindrer også at dere sender det inn på Github
-            // HøyereKlikk på RobinOgMarius -> Manage User Secrets -> Legg inn brukernavn og passord
-            // Brukernavn og passord sendes inn i bodyen til en POST-request
-            // Dette er en form for autentisering som kalles "Resource Owner Password Credentials Grant"
-            // Som forteller APIet hvilken type autentisering som blir brukt...
+            var content = new StringContent($"grant_type=password&username={username}&password={password}", Encoding.UTF8, "application/x-www-form-urlencoded");
 
+            HttpResponseMessage response = await client.PostAsync(url, content);
 
-            using (HttpClient client = new HttpClient())
+            if (response.IsSuccessStatusCode)
             {
-                Console.WriteLine("hei");
-                var content = new StringContent($"grant_type=password&username={"Brukernavn"}&password={"Passord"}", Encoding.UTF8, "application/x-www-form-urlencoded");
+                string responseBody = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(responseBody); // Print the response
 
-                HttpResponseMessage response = await client.PostAsync(url, content);
+                // Parse the JSON response
+                JObject jsonResponse = JObject.Parse(responseBody);
+                string accessToken = jsonResponse["access_token"].ToString();
 
-                if (response.IsSuccessStatusCode) // Sjekker om requesten var vellykket
-                {
-                    string responseBody = await response.Content.ReadAsStringAsync();
-                    Console.WriteLine(responseBody);
-                }
-                else
-                {
-                    Console.WriteLine($"Error: {response.StatusCode}");
-                }
+                return accessToken; // Return the access token
+            }
+            else
+            {
+                Console.WriteLine($"Error: {response.StatusCode}");
+                return null; // Return null if request fails
             }
         }
     }
